7edebb358963220d86b537dc3500bca8c7ceb3a9
MATEN0920 <yjyoon0920@gmail.com>
2026-03-06 15:50:41 +0900

    MyRunner.java edit

--- src/main/java/com/basic/myspringboot/MySpringBootApplication.java
+++ src/main/java/com/basic/myspringboot/MySpringBootApplication.java
@@ -8,18 +8,19 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class MySpringBootApplication {
 
-	public static void main(String[] args) {
-///		SpringApplication.run(MySpringBootApplication.class, args);
+    public static void main(String[] args) {
+
+//        SpringApplication.run(MySpringBootApplication.class, args);
         SpringApplication application = new SpringApplication(MySpringBootApplication.class);
-        application.setWebApplicationType(WebApplicationType.NONE);
+        //어플리케이션 타입 설정하기
+        application.setWebApplicationType(WebApplicationType.SERVLET);
         application.run(args);
+
     }
 
     @Bean
     public String hello() {
-        System.out.println("=======Spring Bean입니다=========");
+        System.out.println("=====Spring Bean 입니다. ====");
         return "Hello Bean";
     }
-
-
 }
