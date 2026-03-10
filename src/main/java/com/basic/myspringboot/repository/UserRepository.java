a60010cc53a91d6c4ffcb2e238cdaecfb9e0a0c2
MATEN0920 <yjyoon0920@gmail.com>
2026-03-10 15:55:23 +0900

    UserRepository add

--- src/main/java/com/basic/myspringboot/repository/UserRepository.java
+++ src/main/java/com/basic/myspringboot/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.basic.myspringboot.repository;
 
-public interface UserRepository {
+import com.basic.myspringboot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
 }
