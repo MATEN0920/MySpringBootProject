376e64cc8c6f2e91f906d6bb61e47e0a5c6cb0b0
MATEN0920 <yjyoon0920@gmail.com>
2026-03-10 16:48:13 +0900

    UserRestController update

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -1,10 +1,13 @@
 package com.basic.myspringboot.controller;
 
+import com.basic.myspringboot.entity.User;
 import com.basic.myspringboot.repository.CustomerRepository;
 import com.basic.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,12 @@ public class UserRestController {
 //        log.info("UserRepository 구현클래스명 = {}",userRepository.getClass().getName());
 //        this.userRepository = userRepository;
 //    }
+
+    //User 등록
+    @PostMapping
+    public User create(@RequestBody User userDetail) {
+        return userRepository.save(userDetail);
+    }
 
 
 }
